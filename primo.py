8a4c1874c642f1cd9a7e8ddaa8bf8b328092e1e6
marcolino01 <159887273+marcolino01@users.noreply.github.com>
2024-02-14 16:48:55 +0100

    Add files via upload

--- primo.py
+++ primo.py
@@ -1,4 +1,4 @@
-from cmath import asin, cos, sin, sqrt
+from math import asin, cos, sin, sqrt
 from math import radians
 
 
@@ -9,9 +9,9 @@ print(10**10)
 print(sqrt(10))
 
 r=6371
-λ1=radians(59.9)
-ϕ1 =radians(10.8)
-λ2=radians(49.3) 
-ϕ2=radians(-123.1)
-d=2*r*asin(sqrt((sin(1/2*(ϕ2-ϕ1))**2)+cos(ϕ1)*cos(ϕ2)*(sin(1/2*(λ2-λ1))**2)))
+ϕ1=radians(59.9)
+λ1 =radians(10.8)
+ϕ2=radians(49.3) 
+λ2=radians(-123.1)
+d=2*r*asin(sqrt(sin(1/2*(ϕ2-ϕ1))**2 + cos(ϕ1)*cos(ϕ2)*sin(1/2*(λ2-λ1))**2))
 print(d)
